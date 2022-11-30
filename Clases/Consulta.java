d191d2a2cf92f9b33b21db3ea6e67184922c9d93
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2022-11-30 17:21:37 -0300

    Update Consulta.java

--- Clases/Consulta.java
+++ Clases/Consulta.java
@@ -2,7 +2,7 @@ package Clases;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-public record Consulta(Date fecha, Diagnostico d, Paciente p) {
+public record Consulta(Date fecha, Diagnostico diagnostico, Medico medico) {
     public String toString(){
         SimpleDateFormat sdf=new SimpleDateFormat("d-M-y");
         return sdf.format(fecha);
