7ea12780cb8a704cb9276951d53f6bcf10a04ced
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2022-12-06 19:46:17 -0300

    Update Medicamento.java

--- Clases/Medicamento.java
+++ Clases/Medicamento.java
@@ -1,27 +1,4 @@
 package Clases;
 import java.util.*;
 
-public class Medicamento {
-    private String id,nombre;
-    private ArrayList<String> composicion;
-    public Medicamento(String id, String nombre, int frecuencia, int dosis, Tratamiento t){
-        this.t=t;
-        this.id=id;
-        this.nombre=nombre;
-        this.frecuencia=frecuencia;
-        this.dosis=dosis;
-        composicion=new ArrayList();
-    }
-    public void addComposicion(String cmp){
-        composicion.add(cmp);
-    }
-    public List<String> getComposicion(){
-        return Collections.unmodifiableList(composicion);
-    }
-    public String getID(){
-        return id;
-    }
-    public String getNombre(){
-        return nombre;
-    }
-}
+public record Medicamento(String id, String nombre, String composicion){}
