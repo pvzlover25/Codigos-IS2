512c867f81a92ec805287b39d9e167b8076edd5a
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2022-12-07 22:47:42 -0300

    Update Ingesta.java

--- Clases/Ingesta.java
+++ Clases/Ingesta.java
@@ -2,10 +2,10 @@ package Clases;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-public record Ingesta(Date fecha, Date hora){
+public record Ingesta(Date dia, Date hora){
     public String toString(){
         SimpleDateFormat sdf=new SimpleDateFormat("d-M-y");
-        String ret=sdf.format(fecha)+" ";
+        String ret=sdf.format(dia)+" ";
         sdf=new SimpleDateFormat("H:mm");
         ret+=sdf.format(hora);
         return ret;
