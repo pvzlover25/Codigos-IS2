a8e2217b0884170f83492c75e556f2bc7c135504
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2022-12-07 22:46:09 -0300

    Update Diagnostico.java

--- Clases/Diagnostico.java
+++ Clases/Diagnostico.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 public class Diagnostico {
     private String prescripcion,deteccion_enfermedad;
-    private Tratamiento t;
+    private HashMap<String,Tratamiento> tratamientos;
     private ArrayList<String> observaciones;
-    public Diagnostico(String prescripcion, String deteccion_enfermedad, Tratamiento t){
+    public Diagnostico(String prescripcion, String deteccion_enfermedad){
         this.prescripcion=prescripcion;
         this.deteccion_enfermedad=deteccion_enfermedad;
-        this.t=t;
+        tratamientos=new HashMap();
         observaciones=new ArrayList();
     }
     public String getDeteccionEnfermedad(){
@@ -23,7 +23,10 @@ public class Diagnostico {
     public List<String> getObservaciones(){
         return Collections.unmodifiableList(observaciones);
     }
-    public Tratamiento getTratamiento(){
-        return t;
+    public void addTratamiento(Tratamiento t){
+       tratamientos.put(t.getID(), t);
     }
+    public Tratamiento getTratamiento(String id_tr){
+        return tratamientos.get(id_tr);
+    } 
 }
