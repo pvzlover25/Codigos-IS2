4b5acabc9b801f8d6289c7341d6404849f08508a
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2022-12-07 23:09:39 -0300

    Create Medico.java

---
+++ Clases/Medico.java
@@ -0,0 +1,34 @@
+package Clases;
+import java.util.*;
+
+public class Medico{
+    private String rut,nombre;
+    private CatalogoMedicamento catalogo;
+    private Consulta c;
+    public Medico(String rut, String nombre, Diagnostico d){
+        this.rut=rut;
+        this.nombre=nombre;
+        catalogo=new CatalogoMedicamento();
+        c=new Consulta(new Date(),d);
+    }
+    public void ingresarTratamiento(String id, Date fecha_inicio, Date fecha_termino){
+        Diagnostico dg=c.diagnostico();
+        dg.addTratamiento(new Tratamiento(fecha_inicio,fecha_termino,id));
+    }
+    public void ingresarMedPrescrito(String id_med, String id_mp, int frecuencia, int dosis, String id_tr){
+        Medicamento med=catalogo.encontrarMed(id_med);
+        Diagnostico dg=c.diagnostico();
+        Tratamiento tr=dg.getTratamiento(id_tr);
+        tr.addPrescripcion(new MedicamentoPrescrito(med,frecuencia,dosis,id_mp));
+    }
+    public double consultarAdherenciaGlobal(Date fecha1, Date fecha2, int frecuencia, String id_tr, String id_mp){
+        Diagnostico dg=c.diagnostico();
+        Tratamiento tr=dg.getTratamiento(id_tr);
+        return tr.consultarAdherenciaGlobal(fecha1, fecha2, frecuencia);
+    }
+    public double consultarAdherenciaDetallada(int umbral, Date fecha1, Date fecha2, int frecuencia, String id_tr){
+        Diagnostico dg=c.diagnostico();
+        Tratamiento tr=dg.getTratamiento(id_tr);
+        return tr.consultarAdherenciaDetallada(umbral, fecha1, fecha2, frecuencia);
+    }
+}
